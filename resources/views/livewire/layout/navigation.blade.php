436fd38631990285bdc8d82e7c88d5aa42696e53
DAW <juanmiguel.valle@iesdonana.org>
2025-05-20 17:24:52 +0200

    se añade producto al carrito

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -30,8 +30,8 @@ new class extends Component
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('inicio')" :active="request()->routeIs('inicio')" wire:navigate>
+                        {{ __('Inicio') }}
                     </x-nav-link>
 
                     @if (auth()->user())
--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -36,7 +36,7 @@ new #[Layout('layouts.guest')] class extends Component
 
         Auth::login($user);
 
-        $this->redirect(route('dashboard', absolute: false), navigate: true);
+        $this->redirect(route('inicio', absolute: false), navigate: true);
     }
 }; ?>
 
